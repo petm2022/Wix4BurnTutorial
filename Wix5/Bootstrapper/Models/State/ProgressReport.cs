3c45cbb0290353d4fa7ffd44db4af1640513fd1f
Peter Møgeltoft <petm@demant.com>
2026-04-28 14:01:22 +0200

    Delete Wix5 and Core

--- Wix5/Bootstrapper/Models/State/ProgressReport.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Bootstrapper.Models.State
-{
-  internal class ProgressReport
-  {
-    public string Message { get; set; }
-    public string PackageName { get; set; }
-    public int Progress { get; set; }
-  }
-}
